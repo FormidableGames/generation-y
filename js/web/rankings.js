0664be3d5434bb0dc8be0346755586b396ebf64d
svalverdet <32246322+svalverdet@users.noreply.github.com>
2018-12-05 09:26:34 +0100

    puntuación final arreglada

--- js/web/rankings.js
+++ js/web/rankings.js
@@ -26,8 +26,13 @@ $(document).ready(function() {
     localStorage.removeItem('chirps');
     localStorage.setItem('chirps', JSON.stringify(testchirps));*/
 
-    var chirps = JSON.parse(localStorage.getItem('chirps')); // Cogemos los chirps del localstorage
+    var chirps_string = JSON.parse(localStorage.getItem('chirps')); // Cogemos los chirps del localstorage
     var rankingDiv = $("#rankings");                        // y el div donde vamos a colocarlos
+    var chirps = [];
+
+    for (i = 0; i < chirps_string.length; i++) {
+        chirps.push(JSON.parse(chirps_string[i]));
+    }
 
     chirps.sort(function(a, b) {
         return b.score - a.score;                           // Ordenamos la lista
@@ -35,7 +40,7 @@ $(document).ready(function() {
 
     // Por cada elemento añadimos un div "twit"
     for (i = 0; i < chirps.length; i++) {
-        var veryLongString = '<div class="content-block tweet"> <div class="tweet-author"><img src="img/web/avatar.png"><span>@yenyen</span><span class="points-num">#'+ (i+1) + ' - ' +chirps[i].score+'pts</span></div><div class="tweet-content">'+chirps[i].chirp_txt+'</div><div class="tweet-feedback"><span class="glyphicon glyphicon-retweet rts"></span><span class="feedback-num rts">69</span> people have rechirped this<br><span class="glyphicon glyphicon-fire likes"></span><span class="feedback-num likes">420</span> people have found this<i>lit af</i></div></div>';
+        var veryLongString = '<div class="content-block tweet"> <div class="tweet-author"><img src="img/web/avatar.png"><span>@protagonistaEdgy</span><span class="points-num">#'+ (i+1) + ' - ' +chirps[i].score+'pts</span></div><div class="tweet-content">'+chirps[i].chirp_info+"<br/>"+chirps[i].chirp_txt+'</div><div class="tweet-feedback"><span class="glyphicon glyphicon-retweet rts"></span><span class="feedback-num rts">69</span> people have rechirped this<br><span class="glyphicon glyphicon-fire likes"></span><span class="feedback-num likes">420</span> people have found this<i>lit af</i></div></div>';
         rankingDiv.append(veryLongString);
     }
 
@@ -44,7 +49,7 @@ $(document).ready(function() {
                 <div class="content-block tweet">
                     <div class="tweet-author">
                         <img src="img/web/avatar.png">
-                        <span>@yenyen</span>
+                        <span>@protagonistaEdgy</span>
                         <span class="points-num">902834 pts</span>
                     </div>
                     <div class="tweet-content">
